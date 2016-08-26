fd3e3d1de0bb9c92c5bcf4b79d893f0640895782
Bruce LANE <bruce@batchass.fr>
2016-08-26 16:48:35 +0200

    more cmd line

--- install.sh
+++ install.sh
@@ -11,12 +11,25 @@ echo Cinder-MIDI2
 git clone https://github.com/brucelane/Cinder-MIDI2 ../../Cinder/blocks/Cinder-MIDI2
 echo Cinder-WebSocketPP
 git clone https://github.com/videodromm/Cinder-WebSocketPP ../../Cinder/blocks/Cinder-WebSocketPP
-echo Cinder-WebSocketPP
-git clone https://github.com/videodromm/assets ..
-echo Required dependencies checked out
 echo Pull changes
 git pull
+echo assets
+git clone https://github.com/videodromm/assets ..
+echo videodromm-controller-cinder
+git clone https://github.com/videodromm/videodromm-controller-cinder ..
+cd ../videodromm-controller-cinder
+git pull
+echo videodromm-livecoding-cinder
+git clone https://github.com/videodromm/videodromm-livecoding-cinder ..
+cd ../videodromm-livecoding-cinder
+git pull
+echo videodromm-visualizer-cinder
+git clone https://github.com/videodromm/videodromm-visualizer-cinder ..
+cd ../videodromm-visualizer-cinder
+git pull
+echo Required dependencies checked out
 cd ../../Cinder/blocks/Cinder-Hap2
 git pull
 cd ../Cinder-Videodromm
 git pull
+pause
